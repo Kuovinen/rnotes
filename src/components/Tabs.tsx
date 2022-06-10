0036ad11db3d335e819711b76ec8ea955f8d427c
RomanZujev <roman.zujev@gmail.com>
2022-06-10 22:59:44 +0300

    initial carousel and button style userselects and cursor cahnges

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -2,11 +2,17 @@ import "./Tabs.css";
 
 function Tabs() {
   return (
-    <div className="Tabs">
-      {" "}
-      <div className="tab">MUSIC</div>
-      <div className="tab">CODE</div>
-      <div className="tab">NOTES</div>
+    <div className="carousel">
+      <div className="Tabs">
+        <div className="tab">MUSIC</div>
+        <div className="tab">CODE</div>
+        <div className="tab">NOTES</div>
+      </div>
+      <div className="Tabs">
+        <div className="tab">MUSIC</div>
+        <div className="tab">CODE</div>
+        <div className="tab">NOTES</div>
+      </div>
     </div>
   );
 }
