901a7d10b71481cb1c61cf8c98726ad68f967e4a
RomanZujev <roman.zujev@gmail.com>
2022-06-10 22:25:18 +0300

    input handled

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,19 @@
+import React from "react";
 import "./Input.css";
 function Input() {
+  const [note, setNote] = React.useState<string>("");
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setNote(e.target.value);
+  }
+
   return (
     <form className="Noteinput">
-      <input className="Notetext" type="text" />
+      <input
+        className="Notetext"
+        type="text"
+        value={note}
+        onChange={(e) => handleInput(e)}
+      />
       <button type="submit" className="makeNoteBtn">
         MAKE NOTE
       </button>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,18 @@
+import React from "react";
 import "./Menu.css";
 function Menu() {
+  const [note, setNote] = React.useState<string>("");
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setNote(e.target.value);
+  }
   return (
     <form className="menu">
-      <input type="text" className="Tabtext" />
+      <input
+        type="text"
+        className="Tabtext"
+        value={note}
+        onChange={(e) => handleInput(e)}
+      />
       <button className="makeTabBtn">NEW TAB</button>
     </form>
   );
