0736e2b275af52b419c290f8427272f4a399e8fb
RomanZujev <roman.zujev@gmail.com>
2022-06-10 23:12:59 +0300

    added state NOTES and TABS, and simulated NOTES mapping in the NOTES tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import React from "react";
 import "./App.css";
 import Menu from "./components/Menu";
 import Main from "./components/Main";
 import Tabs from "./components/Tabs";
 function App() {
+  const [notes, setNotes] = React.useState<string[]>([]);
+  const [tabs, setTabs] = React.useState<string[]>([]);
   return (
     <div className="App">
       <Menu />
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,23 +1,35 @@
+import React from "react";
 import "./Notes.css";
 interface props1 {
+  key: number;
   txt: string;
 }
 function Note(props: props1) {
+  const id: string = "1";
+
+  function handleDelete(id: string) {
+    console.log("Should delete:" + id);
+  }
   return (
     <div className="note">
       <div className="nTxt">{props.txt}</div>
       <div className="nX">
-        <button className="deleteNote">X</button>
+        <button className="deleteNote" onClick={() => handleDelete(id)}>
+          X
+        </button>
       </div>
     </div>
   );
 }
 
 function Input() {
+  //temp notes should be the App's prop or maybe even move the whole future fetch request into this element.
+  const [tempNotes, setTempNotes] = React.useState<string[]>(["one", "two"]);
   return (
     <div className="Notes">
-      <Note txt={"My text"} />
-      <Note txt={"My text2"} />
+      {tempNotes.map((element, index) => {
+        return <Note key={index} txt={"My text " + element} />;
+      })}
     </div>
   );
 }
