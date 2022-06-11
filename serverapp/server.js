01fc40c77ab27fcb11199133cf6fe3159d12f793
RomanZujev <roman.zujev@gmail.com>
2022-06-11 20:45:18 +0300

    added cors

--- serverapp/server.js
+++ serverapp/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 //Add tab
 app.get("/addtab", (req, res) => {
+  console.log("got here");
   res.send("adding tab");
 });
 //Add note
