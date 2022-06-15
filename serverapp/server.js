34003e7647b54050e12f88fcdc87d65a46f747c8
RomanZujev <roman.zujev@gmail.com>
2022-06-16 00:18:08 +0300

    Added connection data properly

--- serverapp/server.js
+++ serverapp/server.js
@@ -1,28 +1,54 @@
 const express = require("express");
 const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-//Add tab
-app.post("/addtab", (req, res) => {
-  console.log(req.body.payload);
-  res.send("adding tab");
-});
-//Add note
-app.post("/addnote", (req, res) => {
-  console.log(req.body.payload);
-  res.send("adding note");
-});
-//Remove note
-app.get("/removenote", (req, res) => {
-  res.send("removeing note");
-});
+const connectionString =
+  "mongodb+srv://XXX:YYY@cluster0.u1ysblp.mongodb.net/?retryWrites=true&w=majority";
+async function accessDb() {
+  const client = await MongoClient.connect(connectionString);
+  const db = await client.db("rnotes");
+  const notes = db.collection("notes");
 
-app.listen(port, () => {
-  console.log(`Server app listening on port ${port}`);
-});
+  app.get("/", (req, res) => {
+    db.collection("notes")
+      .find()
+      .toArray()
+      .then((results) => {
+        console.log(results);
+        res.send(JSON.stringify(results));
+      })
+      .catch((error) => console.error(error));
+
+    // ...
+  });
+  //Add tab
+  app.post("/addtab", (req, res) => {
+    console.log(req.body.payload);
+    res.send("adding tab");
+  });
+  //Add note
+  app.post("/addnote", (req, res) => {
+    notes
+      .insertOne(req.body)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => console.error(error));
+
+    res.send("added note" + req.body.payload);
+  });
+  //Remove note
+  app.get("/removenote", (req, res) => {
+    res.send("removeing note");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server app listening on port ${port}`);
+  });
+}
+accessDb();
