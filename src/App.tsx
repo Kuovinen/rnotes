55cec51a95ac13f8650f071e693a9b8954b428f8
RomanZujev <roman.zujev@gmail.com>
2022-06-15 21:19:47 +0300

    added fetch request and its useEffect, that gets and sets Notes araay

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,28 @@ import Tabs from "./components/Tabs";
 function App() {
   const [notes, setNotes] = React.useState<string[]>([]);
   const [tabs, setTabs] = React.useState<string[]>([]);
+
+  async function getDBdata() {
+    const response = await fetch("http://localhost:4000/");
+    const data = await response.text();
+    const parsedData = JSON.parse(data);
+    console.log(parsedData);
+    setNotes((notes) => [
+      ...notes,
+      ...parsedData.map(
+        (element: { _id: any; payload: string }) => element.payload
+      ),
+    ]);
+  }
+  React.useEffect(() => {
+    getDBdata();
+  }, []);
+
   return (
     <div className="App">
       <Menu />
       <Tabs />
-      <Main />
+      <Main notes={notes} />
     </div>
   );
 }
