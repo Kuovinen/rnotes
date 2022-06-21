0024855cce5c6b7ce038035a84cfa2c4b6002c2e
RomanZujev <roman.zujev@gmail.com>
2022-06-21 22:41:40 +0300

    implemented most of carusel functionality, still need movement to the right and possibly restructure

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,4 +1,5 @@
 import "./Tabs.css";
+import React from "react";
 interface props {
   tabs: string[];
   setNotes: React.Dispatch<
@@ -40,9 +41,56 @@ function Tab(props: props2) {
 
 function Tabs(props: props) {
   console.log("render TABS");
+  const initX = React.useRef<number>(0);
+  const [x, setX] = React.useState<number>(0);
+  const listenerX = React.useRef<number>(0);
+  const width = (window as any).innerWidth;
+  console.log("rerendered TAS, current X is : " + x);
+  const caru = React.useRef<HTMLDivElement>(null);
+  function handleBeginDrag(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    e.stopPropagation();
+
+    const boundry = caru.current?.getBoundingClientRect();
+    //get initial click position
+    initX.current = (window as any).event.clientX;
+    //console.log(boundry?.left);
+    //setX((x) => x + 20);
+  }
+  function handleEndDrag(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    e.stopPropagation();
+    initX.current = 0;
+  }
+  function moveCaru(e: MouseEvent) {
+    e.stopPropagation();
+    //if initial click position isn't 0, then I clicked
+    if (initX.current) {
+      const currentX = (window as any).event.clientX;
+      setX((x) => x + currentX - initX.current);
+      listenerX.current += currentX - initX.current;
+      console.log(x + "/" + listenerX.current);
+      if (listenerX.current < -width) {
+        console.log("X is less that WIDTH!!!");
+        setX(0);
+        listenerX.current = 0;
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    caru.current?.addEventListener("mousemove", (e) => {
+      moveCaru(e);
+    });
+    return caru.current?.removeEventListener("mousemove", (e) => {
+      moveCaru(e);
+    });
+  }, []);
   return (
-    <div className="carousel">
-      <div className="Tabs">
+    <div className="carousel" ref={caru} style={{ left: `${x}px` }}>
+      <div
+        className="Tabs"
+        onMouseDown={(e) => handleBeginDrag(e)}
+        onMouseUp={(e) => handleEndDrag(e)}
+      >
         {props.tabs.map((element, index) => (
           <Tab
             key={index}
