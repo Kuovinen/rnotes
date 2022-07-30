87653b9d8d9b62814ba43d7a16e98487249c057e
RomanZujev <roman.zujev@gmail.com>
2022-07-30 18:46:05 +0300

    added proper comments foramting and notes display upon init

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import Main from "./components/Main";
 import Tabs from "./components/Tabs";
 function App() {
   console.log("render APP!----------------");
+  /*______________________________________________________________________________
+________________________________________________________________________ STATE*/
   const [notes, setNotes] = React.useState<{ _id: string; payload: string }[]>(
     []
   );
   const [tabs, setTabs] = React.useState<string[]>([]);
   const currentTab = React.useRef("notes");
   const initialRender = React.useRef(true);
+  /*______________________________________________________________________________
+____________________________________________________________________ FUNCTIONS*/
+  //set tabs
   async function getDBdata() {
     const response = await fetch("http://localhost:4000/");
     const data = await response.text();
@@ -19,7 +24,30 @@ function App() {
     console.log(parsedData);
 
     setTabs(() => [...parsedData]);
+    //if the tabs list came non empy, use first value to request it's notes
+    if (parsedData.length > 0) {
+      getList(parsedData[0]);
+    }
   }
+  //now set notes
+  async function getList(value: string) {
+    const response = await fetch("http://localhost:4000/getnotes", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ payload: value }),
+    });
+    const data = await response.text();
+    const parsedData = JSON.parse(data);
+    console.log("got new notes");
+    console.log(parsedData);
+    setNotes(() => [...parsedData]);
+  }
+  /*______________________________________________________________________________
+______________________________________________________________________ EFFECTS*/
+  //Get inital data for page load
   React.useEffect(() => {
     if (initialRender.current) {
       getDBdata();
@@ -27,6 +55,8 @@ function App() {
     }
   }, []);
 
+  /*______________________________________________________________________________
+_______________________________________________________________________ RETURN*/
   return (
     <div className="App">
       <Menu />
